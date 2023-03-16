badab307ff8bbbb9d60d4900cdc8f6c124aea03f
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2023-03-16 23:54:07 +0100

    groups

--- scores/pages/batch_Crop.sh
+++ scores/pages/batch_Crop.sh
@@ -1,3 +1,5 @@
-lilypond -fpng ../bwv-1004_5.ly && sips -r 90 *.png && mkdir -p cropped
-for f in *.png; do ffmpeg -y -i "${f}" -vf  "crop=1100:96:80:12" "cropped/${f%%.*}.png"; done
-ffmpeg -y -i "bwv-1004_5-page1.png"    -vf  "crop=1150:96:30:12" "cropped/bwv-1004_5-page1.png"
+lilypond -fpng ../bwv-1004_5_for_PNGs.ly && sips -r 90 *.png && mkdir -p cropped
+for f in *.png; do ffmpeg -y -i "${f}" -vf  "crop=1100:96:80:12" "cropped/${f%%.*}.jpg"; done
+ffmpeg -y -i "bwv-1004_5_for_PNGs-page1.png"    -vf  "crop=1143:96:37:12" "cropped/bwv-1004_5_for_PNGs-page1.jpg"
+ffmpeg -y -i "bwv-1004_5_for_PNGs-page34.png"   -vf  "crop=1143:96:37:12" "cropped/bwv-1004_5_for_PNGs-page34.jpg"
+ffmpeg -y -i "bwv-1004_5_for_PNGs-page53.png"   -vf  "crop=1143:96:37:12" "cropped/bwv-1004_5_for_PNGs-page53.jpg"
