c0b95d7e1c01e84d43adfb0885976b12f81f0ae0
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2023-03-18 18:57:47 +0100

    a bit larger score

--- scores/pages/batch_Crop.sh
+++ scores/pages/batch_Crop.sh
@@ -1,8 +1,8 @@
 lilypond -fpng ../bwv-1004_5_for_PNGs.ly && sips -r 90 *.png && mkdir -p cropped && mkdir -p cropped_transparent
-for f in *.png; do ffmpeg -y -i "${f}" -vf  "crop=1095:96:85:12,colorkey=white:0.3:0.5" "cropped_transparent/${f%%.*}.png"; done
-ffmpeg -y -i "bwv-1004_5_for_PNGs-page1.png"    -vf  "crop=1142:96:38:12,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page1.png"
-ffmpeg -y -i "bwv-1004_5_for_PNGs-page34.png"   -vf  "crop=1142:96:38:12,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page34.png"
-ffmpeg -y -i "bwv-1004_5_for_PNGs-page53.png"   -vf  "crop=1142:96:38:12,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page53.png"
+for f in *.png; do ffmpeg -y -i "${f}" -vf  "crop=1095:78:85:22,colorkey=white:0.3:0.5" "cropped_transparent/${f%%.*}.png"; done
+ffmpeg -y -i "bwv-1004_5_for_PNGs-page1.png"    -vf  "crop=1142:78:38:22,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page1.png"
+ffmpeg -y -i "bwv-1004_5_for_PNGs-page34.png"   -vf  "crop=1142:78:38:22,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page34.png"
+ffmpeg -y -i "bwv-1004_5_for_PNGs-page53.png"   -vf  "crop=1142:78:38:22,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page53.png"
 
 
 # 
