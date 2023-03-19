411de193afbdb1dc0bdfb7dc2add4b62f7954032
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2023-03-19 10:04:02 +0100

    run lilypond thru docker

--- scores/pages/batch_Crop.sh
+++ scores/pages/batch_Crop.sh
@@ -5,3 +5,5 @@ for f in *.png; do ffmpeg -y -i "${f}" -vf  "crop=1095:78:85:22,colorkey=white:0
 ffmpeg -y -i "bwv-1004_5_for_PNGs-page1.png"    -vf  "crop=1142:78:38:22,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page1.png"
 ffmpeg -y -i "bwv-1004_5_for_PNGs-page34.png"   -vf  "crop=1142:78:38:22,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page34.png"
 ffmpeg -y -i "bwv-1004_5_for_PNGs-page53.png"   -vf  "crop=1142:78:38:22,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page53.png"
+
+docker run -v $(pwd):/app -w /app jeandeaual/lilypond:2.25.2 lilypond -dno-point-and-click -fpng bwv-1004_5_for_PNGs.ly && mv *.png pages
