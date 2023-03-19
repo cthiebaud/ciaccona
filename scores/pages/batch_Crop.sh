5506c7d361a1983ba12f9303af64a10c050471d6
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2023-03-19 08:53:55 +0100

    clean

--- scores/pages/batch_Crop.sh
+++ scores/pages/batch_Crop.sh
@@ -1,17 +1,7 @@
-lilypond -fpng ../bwv-1004_5_for_PNGs.ly && sips -r 90 *.png && mkdir -p cropped && mkdir -p cropped_transparent
+mkdir -p cropped 
+mkdir -p cropped_transparent
+lilypond -fpng ../bwv-1004_5_for_PNGs.ly && sips -r 90 *.png
 for f in *.png; do ffmpeg -y -i "${f}" -vf  "crop=1095:78:85:22,colorkey=white:0.3:0.5" "cropped_transparent/${f%%.*}.png"; done
 ffmpeg -y -i "bwv-1004_5_for_PNGs-page1.png"    -vf  "crop=1142:78:38:22,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page1.png"
 ffmpeg -y -i "bwv-1004_5_for_PNGs-page34.png"   -vf  "crop=1142:78:38:22,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page34.png"
 ffmpeg -y -i "bwv-1004_5_for_PNGs-page53.png"   -vf  "crop=1142:78:38:22,colorkey=white:0.3:0.5" "cropped_transparent/bwv-1004_5_for_PNGs-page53.png"
-
-
-# 
-ffmpeg -f lavfi -i color=c=white@0.5:s=1920x1080,format=rgba -i "bwv-1004_5_for_PNGs-page1.png" -shortest -filter_complex "[1:v]colorkey=white[ckout];[0:v][ckout]overlay[out]" -map "[out]" out.png
-
-for f in *.png; do ffmpeg -y -i "${f}" -vf  "crop=1095:96:85:12" "cropped/${f%%.*}.jpg"; done
-
-ffmpeg -i "bwv-1004_5_for_PNGs-page1.png" -vf colorkey=white:0.3:0.5 out.png
-
-# for f in *.jpg; do magick "${f}" -fill "rgba(255,255,255,0.5)" -opaque #ffffff "${f%%.*}.png"; done
-
-# for f in *.jpg; do echo "${f%%.*}.png"; done
