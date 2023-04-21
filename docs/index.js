fbce66c8fa7d8370c5ee9716bc87efd8f4e8e5f5
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2023-04-21 09:41:12 +0200

    version bump

--- docs/index.js
+++ docs/index.js
@@ -1,11 +1,11 @@
 import isotopeLayout from 'https://cdn.jsdelivr.net/npm/isotope-layout@3.0.6/+esm'
-import config from "/js/config.js?v=0.8.24"
-import codec from "/js/structure.js?v=0.8.24"
-import createColoredBadges from "/js/colors.js?v=0.8.24"
-import createTimings from "/js/timings.js?v=0.8.24"
-import resizeSVGs from "/js/resizeSVGs.js?v=0.8.24"
-import createPlayer from "/js/player.js?v=0.8.24"
-import Ω from "/js/jquery.js?v=0.8.24"
+import config from "/js/config.js?v=0.8.25"
+import codec from "/js/structure.js?v=0.8.25"
+import createColoredBadges from "/js/colors.js?v=0.8.25"
+import createTimings from "/js/timings.js?v=0.8.25"
+import resizeSVGs from "/js/resizeSVGs.js?v=0.8.25"
+import createPlayer from "/js/player.js?v=0.8.25"
+import Ω from "/js/jquery.js?v=0.8.25"
 
 // transform windows loaded event to promise
 const windowLoaded = new Promise((resolve) => {
