bc67c4cff59d5e36899b104e93c9eebdaa44d2b8
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2023-04-26 08:07:31 +0200

    bump version

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,5 +1,5 @@
-import { getCookie, setCookie, removeCookie } from "/js/utils.js?v=0.9.6"
-import codec from "/js/structure.js?v=0.9.6"
+import { getCookie, setCookie, removeCookie } from "/js/utils.js?v=0.9.7"
+import codec from "/js/structure.js?v=0.9.7"
 
 class Config {
     #scoreDisplay = 'firstBar'
--- docs/js/jquery.js
+++ docs/js/jquery.js
@@ -1,7 +1,7 @@
 import jquery from 'https://cdn.jsdelivr.net/npm/jquery@3.6.4/+esm'
-import config from "/js/config.js?v=0.9.6"
-import codec from "/js/structure.js?v=0.9.6"
-import { shuffleArray } from "/js/utils.js?v=0.9.6"
+import config from "/js/config.js?v=0.9.7"
+import codec from "/js/structure.js?v=0.9.7"
+import { shuffleArray } from "/js/utils.js?v=0.9.7"
 
 const $ = jquery
 
